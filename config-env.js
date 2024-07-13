2b72790f7ab21b1e6a0a80bd94368544b84130f1
okidusehansa <137091812+okidusehansa@users.noreply.github.com>
2024-07-13 17:23:20 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -1,6 +1,6 @@
 const envv2 = require('./config-v2')
 var GITHUB_TOKEN;
-var BOT_NUMBER;
+var BOT_NUMBER;+94715248600
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -14,18 +14,17 @@ GITHUB_TOKEN = process.env.GITHUB_AUTH_TOKEN
 }else{
 GITHUB_TOKEN = envv2.GITHUB_AUTH_TOKEN
 }
-if(process.env.BOT_NUMBER){
-BOT_NUMBER = process.env.BOT_NUMBER
-}else{
+if(process.env.BOT_NUMBER){+94715248600
+BOT_NUMBER = process.env.BOT_NUMBER}+94715248600{
 BOT_NUMBER = envv2.BOT_NUMBER
-}
+}+94715248600
 
 
 
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,    
-    BOT_NUMBER:  BOT_NUMBER,
-    GITHUB_USERNAME: username,
+    SESSION_ID:  process.env.SESSION_ID,PRABATH-MD~DcFHlLSD#DzJXHhASDfErQEZh56pAWTVa9A3mRTciv6psliEQEMA    
+    BOT_NUMBER:  BOT_NUMBER,+94715248600
+    GITHUB_USERNAME: username,okidu sehansa 
     GITHUB_AUTH_TOKEN: GITHUB_TOKEN,
 };
